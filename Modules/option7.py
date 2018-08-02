a662481e595573fcff1e126d49ac68b0a2746f66
Abhinav Sharma <31707664+abhinavs03@users.noreply.github.com>
2018-08-02 14:52:55 +0530

    Create option7.py

---
+++ Modules/option7.py
@@ -0,0 +1,18 @@
+import cx_Oracle
+conn = cx_Oracle.connect('SYSTEM/SYSTEM')
+cur = conn.cursor()
+
+
+class Staff_View:
+    def __init__(self, staff_id):
+        self.staff_id = staff_id
+
+    def view_details(self):
+        details = []
+        schema = ['Staff ID:', 'Name:', 'Dept:', 'Gender:', 'Address:', 'Telephone:', 'Dr.Code:']
+        query = cur.execute('select * from staff_table where staff_id = :1',{'1':self.staff_id})
+        for i in query.fetchone():
+            details.append(i)
+        for a,b in zip(schema, details):
+            print(a,b)
+        print('\n\n')
