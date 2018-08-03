2b88b41b4e3f776dc0f4cfe5b225dcfb990079c7
lovesharma1998 <32165268+lovesharma1998@users.noreply.github.com>
2018-08-03 11:05:02 +0530

    option2-patient_payment module added

---
+++ Modules/option2.py
@@ -0,0 +1,15 @@
+import cx_Oracle
+con=cx_Oracle.connect("KUSH/KUSH")
+cur=con.cursor()
+class payment:
+    def __init__(self,pat_id):
+        self.pat_id=pat_id
+    def pay_detail(self):
+        details=[]
+        schema=['Pat_Id: ','Amount: ','Transaction_Id: ','PayMode: ','Bill_Date: ']
+        query=cur.execute('select * from doctor where pat_id=:1',{'1':self.pat_id})
+        for i in query.fetchone():
+            details.append(i)
+        for a,b in zip(schema,details):
+            print(a,b)
+            print('\n\n')
