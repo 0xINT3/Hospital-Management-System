f870663b8a4ab7826cfe945e6a61ffbc4cb61b95
lovesharma1998 <32165268+lovesharma1998@users.noreply.github.com>
2018-08-03 13:43:27 +0530

    option1 module

---
+++ Modules/check.py
@@ -0,0 +1,13 @@
+import cx_Oracle
+conn=cx_Oracle.connect('KUSH/KUSH')
+cur=conn.cursor()
+class entry:
+    def __init__(self,pat_id,name,gender,address,tel,dr_code):
+        self.pat_id=pat_id
+        self.name=name
+        self.gender=gender
+        self.address=address
+        self.dr_code=dr_code
+    def enter(self):
+      query=cur.execute("INSERT INTO patient VALUES (:1, :2, :3, :4, :5, :6)",{'1':"\'"+ str(self.pat_id),'2': "\'"+ str(self.name)+ "\'", '3': "\'"+ str(self.gender)+ "\'" ,'4': "\'"+str(self.address)+ "\'" , '5': str(self.tel),'6': "\'"+str(self.dr_code)})
+    
