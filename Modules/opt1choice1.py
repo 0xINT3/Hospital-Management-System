f84982f89c2d94727629a0d4bf0362df5865ffee
lovesharma1998 <32165268+lovesharma1998@users.noreply.github.com>
2018-08-05 16:33:39 +0530

    opt1choice1(schedule appointment)

---
+++ Modules/opt1choice1.py
@@ -0,0 +1,15 @@
+import cx_Oracle
+conn=cx_Oracle.connect('KUSH/KUSH')
+cur=conn.cursor()
+
+class appointment:
+    def __init__(self,pat_id,specialization):
+        self.pat_id=pat_id
+        self.specialization=specialization
+    def schedule(self):
+        if('select * from doctor where self.specialization'):
+            print("Appointment done for",self.specialization)
+        else:
+            print("Unavailability of this type of specialised doctor in this hospital")
+
+            
