e47e19d082bfa1ae3bb77e1bec94a4de467b2041
Abhinav Sharma <31707664+abhinavs03@users.noreply.github.com>
2018-08-11 19:20:33 +0530

    Update option4.py

--- Modules/option4.py
+++ Modules/option4.py
@@ -5,13 +5,16 @@ class receipt:
     def __init__(self,pat_id):
         self.pat_id=pat_id
     def display(self):
-        details=[]
-        schema=['Amount: ','Transaction Id: ','Paymode: ','Bill_date: ']
-        query=cur.execute('select AMOUNT,TRANSCATION_ID,PAYMODE,BILL_DATE from payment where pat_id=:1',{'1':self.pat_id})
-        for i in query.fetchone():
-            details.append(i)
-        for a,b in zip(schema,details):
-            print(a,b)
-            print('\n\n')
-            
-        
+        try:            
+            details=[]
+            schema=['Amount: ','Transaction Id: ','Paymode: ','Bill_date: ']
+            query=cur.execute('select AMOUNT,TRANSCATION_ID,PAYMODE,BILL_DATE from payment where pat_id=:1',{'1':self.pat_id})
+            for i in query.fetchone():
+                details.append(i)
+            for a,b in zip(schema,details):
+                print(a,b)
+                print('\n\n')
+        except:
+            print("No such record found.")
+
+
