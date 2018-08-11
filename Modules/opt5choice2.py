58f99a9c32d283fe79949e7871d883f228d13dea
Abhinav Sharma <31707664+abhinavs03@users.noreply.github.com>
2018-08-11 17:32:41 +0530

    Update opt5choice2.py

--- Modules/opt5choice2.py
+++ Modules/opt5choice2.py
@@ -4,12 +4,12 @@ cur=conn.cursor()
 
 
 class insert:
-    def __init__(self,dr_code,name,gender,address,designation):
+    def __init__(self,dr_code,name,gender,address,specialization):
         self.dr_code = dr_code
         self.name = name
         self.gender = gender
         self.address = address
-        self.designation = designation
+        self.specialization = specialization
     def enter(self):
-        query=cur.execute("INSERT INTO doctor VALUES (:1, :2, :3, :4, :5)",{'1': int(self.dr_code), '2': str(self.name), '3': str(self.gender) ,'4':str(self.address), '5': str(self.designation)})
+        query=cur.execute("INSERT INTO doctor VALUES (:1, :2, :3, :4, :5)",{'1': int(self.dr_code), '2': str(self.name), '3': str(self.gender) ,'4':str(self.address), '5': str(self.specialization)})
         conn.commit()
